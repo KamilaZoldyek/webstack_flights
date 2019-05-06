c960d2a1ebd8b2b96e0463711c402f170a24f130
KamilaZoldyek <kmilawicked@hotmail.com>
2019-05-05 22:27:30 -0300

    search layouts

--- Webstack_Flights/app/src/main/java/com/example/kamilazoldyek/webstack_flights/activity/MainActivity.java
+++ Webstack_Flights/app/src/main/java/com/example/kamilazoldyek/webstack_flights/activity/MainActivity.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.kamilazoldyek.webstack_flights.R;
@@ -34,17 +36,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.test);
 
-        button = findViewById(R.id.button_home);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
-                startActivity(intent);
-            }
-        });
+//        button = findViewById(R.id.button_home);
+//
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+//                startActivity(intent);
+//            }
+//        });
 
     }
 
